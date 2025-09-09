7c0f203a0ecae134e798b334292f681f023b6469
eshashetty19 <eshapshetty@gmail.com>
2025-09-09 12:41:27 +0530

    Add Database

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 import express from "express";
 import bodyParser from "body-parser";
-
-let posts = [
-    {
-        id: 1,
-        title: "My First Post",
-        content: "This is first blog post. Excited to start blogging!",
-        date: new Date()
-    },
-    {
-        id: 2,
-        title: "Second Post",
-        content: "Here is another post to test the blog application.",
-        date: new Date()
-    }
-];
+import pg from "pg";
+import dotenv from "dotenv";
 
 const app = express();
 const port = 3000;
+
+dotenv.config();
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-app.get("/", (req,res) => {
+app.get("/", async(req,res) => {
+    const result = await db.query("SELECT * FROM posts ORDER BY id ASC");
+    const posts = result.rows;
     res.render("index.ejs",{ pos: posts});
 });
 
@@ -31,50 +30,56 @@ app.get("/new", (req,res) => {
     res.render("new.ejs");
 });
 
-app.post("/new",(req,res)=>{
-    
-    const newPost = {
-        id: posts.length + 1,
-        title: req.body['title'], //req.body.title
-        content: req.body['content'], //req.body.content
-        date: new Date()
-    };
-    posts.push(newPost);
+app.post("/new", async(req,res)=>{
+    const title= req.body['title'];//req.body.title
+    const content= req.body['content']; //req.body.content
+    const date= new Date();
+    await db.query("INSERT INTO posts (title, content, date) VALUES($1, $2, $3)",[title, content, date])
     res.redirect("/");
 });
 
-app.get("/edit/:id", (req,res) => {
+app.get("/edit/:id", async(req,res) => {
+    try{
     const postId = parseInt(req.params.id); //req.params.id gets the route parameters if i it fetches it as 1 similar to req.body
     //we convert into integer because its in the form of string but when comparing we need to compare with integer otherwise even if they are equal it gives false
     // Find the post with the matching id
-    const post = posts.find(p => p.id === postId);
+    const result = await db.query("SELECT * FROM POSTS WHERE id=$1", [postId]);
+    const post = result.rows[0];
+   if(post){
+     res.render("edit.ejs", { post: post });
+   }else{
+    res.status(404).send("Post not found");
+   }
     
-    if (post) {
-        res.render("edit.ejs", { post: post });
-    } else {
-        res.status(404).send("Post not found");
+    } catch(error) {
+        console.log(err);
     }
-});
+}
+);
 
-app.post("/edit/:id", (req,res) => {
+app.post("/edit/:id", async(req,res) => {
+    try{
     const postId = parseInt(req.params.id);
-    const post = posts.find(p => p.id === postId);
+    const {title, content} = req.body;
+    const date = new Date();
+     await db.query("UPDATE posts SET title = $1, content = $2, date = $3 WHERE id = $4",[title, content, date, postId]);
 
-    if(post){
-        post.title = req.body.title;
-        post.content=req.body.content;
-        
-    }
    res.redirect("/");
+    }
+    catch(err){
+        console.log(err);
+    }
 });
 
-app.post("/delete/:id", (req,res) => {
+app.post("/delete/:id", async(req,res) => {
+try{
  const postId = parseInt(req.params.id);
- const post=posts.find(p => p.id === postId);
- if(post){
-        posts = posts.filter(p => p.id !== postId);
- }
+ const result = await db.query("DELETE FROM posts WHERE id = $1", [postId]);
  res.redirect("/");
+}
+catch(err){
+    console.log(err);
+}
 });
 
 app.listen(port, ()=> {
